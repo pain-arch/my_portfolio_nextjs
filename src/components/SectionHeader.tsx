a02911e8ca4fbab1683652bc005587e0d70a2a7a
pain-arch <himulol123@gmail.com>
2024-09-09 17:08:46 +0600

    refactor: Add SectionHeader component and use it in ProjectsSection
    feat: Add TestimonialsSection component to Home page

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,29 @@
+export const SectionHeader = ({
+  title,
+  description,
+  eyebrow,
+}: {
+  title: string;
+  description: string;
+  eyebrow: string;
+}
+) => {
+
+
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+          {eyebrow}
+        </p>
+      </div>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+        {title}
+      </h2>
+      <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">
+        {description}
+      </p>
+    </>
+  );
+};
