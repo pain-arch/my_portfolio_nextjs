d221f0e9a4b350523990a60d6c24fb94b896371a
pain-arch <himulol123@gmail.com>
2024-09-16 03:57:28 +0600

    feat: Add ToolboxItems component

---
+++ src/components/ToolboxItems.tsx
@@ -0,0 +1,31 @@
+import { twMerge } from "tailwind-merge";
+import { Techicon } from "./TechIcon";
+
+export const ToolboxItems = ({
+    items,
+    className,
+    itemsWrapperClassName,
+}: {
+  items: {
+    title: string;
+    iconType: React.ElementType;
+    }[];
+    className?: string;
+    itemsWrapperClassName?: string;
+}) => {
+  return (
+      <div className={twMerge("flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,tranparent)]", className)}>
+      <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6", itemsWrapperClassName)}>
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+          >
+            <Techicon component={item.iconType} />
+            <span className="font-semibold">{item.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
